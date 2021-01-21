761562e4f9d960a0d4ca02a15a5fff6f7af776f9
R37r0-Gh057 <40432896+R37r0-Gh057@users.noreply.github.com>
2021-01-21 15:31:55 +0530

    Update config.py

--- core/config.py
+++ core/config.py
@@ -8,21 +8,20 @@ def init():
 	RED = '\033[31;1m'
 	WHITE = '\033[m'
 
-	T_KILL = False
-
 	module_list = [] #for storing list of available modules
 	module_filename_list = [] #for storing filenames of available modules
 	module_desc_list = [] #for storing description of available modules
 
 
 	banner = f'''
-███╗   ███╗   ██╗        ██████╗ 
-████╗ ████║   ██║        ██╔══██╗
-██╔████╔██║   ██║        ██████╔╝
-██║╚██╔╝██║   ██║        ██╔══██╗
-██║ ╚═╝ ██║██╗███████╗██╗██████╔╝
-╚═╝     ╚═╝╚═╝╚══════╝╚═╝╚═════╝ 
-
+███╗   ███╗   ██████╗ ██████╗ 
+████╗ ████║   ██╔══██╗██╔══██╗
+██╔████╔██║   ██████╔╝██████╔╝
+██║╚██╔╝██║   ██╔═══╝ ██╔══██╗
+██║ ╚═╝ ██║██╗██║██╗  ██████╔╝
+╚═╝     ╚═╝╚═╝╚═╝╚═╝  ╚═════╝ 
+                               
+                               
 [*] {RED}Author: {WHITE}R37r0-Gh057
 [*] {GREEN}Github: {WHITE}https://github.com/R37r0-Gh057
 [*] {BLUE}Telegram: {WHITE}@R37R0_GH057
